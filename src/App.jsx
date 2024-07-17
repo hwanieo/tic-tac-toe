176f89c60b2041a7b2595acd4417ab5a92f4134d
hwnii <hwniieo@gmail.com>
2024-07-17 16:48:59 +0900

    전역으로 사용할 상태 리팩토링

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,50 @@
+import { useState } from 'react';
 import GameBoard from './components/GameBoard';
 import Player from './components/Player';
+import Log from './components/Log';
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
+  const [activePlayer, setActivePlayer] = useState('X');
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setActivePlayer((curActivePlayer) => (curActivePlayer === 'X' ? 'O' : 'X'));
+    setGameTurns((prevTurns) => {
+      let currentPlayer = 'X';
+
+      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
+        currentPlayer = 'O';
+      }
+
+      const updateTurns = [
+        // currentPlayer 대신 activePlayer를 이곳에서 사용하는 것은 aP에게 있어서 이것은 다른 상태다. 이상적이지 않아.
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+
+      return updateTurns;
+    });
+  }
+
   return (
-    <div id='game-container'>
-      <ol id='players' className='highlight-player'>
-        <Player initialName={'Player 1'} symbol={'O'} />
-        <Player initialName={'Player 2'} symbol={'X'} />
-      </ol>
-      <GameBoard />
-    </div>
+    <main>
+      <div id='game-container'>
+        <ol id='players' className='highlight-player'>
+          <Player
+            initialName={'Player 1'}
+            symbol={'O'}
+            isActive={activePlayer === 'O'}
+          />
+          <Player
+            initialName={'Player 2'}
+            symbol={'X'}
+            isActive={activePlayer === 'X'}
+          />
+        </ol>
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+      </div>
+      <Log />
+    </main>
   );
 }
 
