86a67ac668436280a57c1e5d035f4e65a4f50c79
hwnii <hwniieo@gmail.com>
2024-07-17 17:41:05 +0900

    조건적 버튼 비활성화

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,27 @@ import GameBoard from './components/GameBoard';
 import Player from './components/Player';
 import Log from './components/Log';
 
+function deriveActivePlayer(gameTurns) {
+  // 이렇게 플레이어 심볼까지 조작이 한 번에 되는데 이걸 상태로 관리할 필요는 없다.
+  let currentPlayer = 'X';
+
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+
+  return currentPlayer;
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('X');
+  // const [activePlayer, setActivePlayer] = useState('X');
+
+  const activePlayer = deriveActivePlayer(gameTurns);
 
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((curActivePlayer) => (curActivePlayer === 'X' ? 'O' : 'X'));
+    // setActivePlayer((curActivePlayer) => (curActivePlayer === 'X' ? 'O' : 'X'));
     setGameTurns((prevTurns) => {
-      let currentPlayer = 'X';
-
-      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-        currentPlayer = 'O';
-      }
+      const currentPlayer = deriveActivePlayer(prevTurns);
 
       const updateTurns = [
         // currentPlayer 대신 activePlayer를 이곳에서 사용하는 것은 aP에게 있어서 이것은 다른 상태다. 이상적이지 않아.
@@ -32,18 +41,18 @@ function App() {
         <ol id='players' className='highlight-player'>
           <Player
             initialName={'Player 1'}
-            symbol={'O'}
-            isActive={activePlayer === 'O'}
+            symbol={'X'}
+            isActive={activePlayer === 'X'}
           />
           <Player
             initialName={'Player 2'}
-            symbol={'X'}
-            isActive={activePlayer === 'X'}
+            symbol={'O'}
+            isActive={activePlayer === 'O'}
           />
         </ol>
         <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
-      <Log />
+      <Log turns={gameTurns} />
     </main>
   );
 }
